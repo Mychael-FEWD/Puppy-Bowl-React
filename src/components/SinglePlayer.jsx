266f926037bb7036de088d5ce55a53202b7e0c51
fmagmych <mychaelmagnuson@gmail.com>
2023-08-03 11:08:49 -0500

    Building a general component that looks well with all the different individual player datas and images and displays team name and breed

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,7 +1,19 @@
-export default function SinglePlayer() {
+export default function SinglePlayer({ singlePlayer }) {
+  function upperCaseName(string) {
+    const newName = string.charAt(0).toUpperCase() + string.slice(1);
+    return newName;
+  }
+
   return (
-    <>
-      <h1>Single Player</h1>
-    </>
+    <section id="single-player">
+      <div className="single-player-img-cont">
+        <img className="single-player-img" src={singlePlayer.imageUrl} />
+      </div>
+      <div className="single-player-data">
+        <h1>{upperCaseName(singlePlayer.name)}</h1>
+        <h2>Currently on-{singlePlayer.status}</h2>
+        <h2>Team: {singlePlayer.team && singlePlayer.team.name}</h2>
+      </div>
+    </section>
   );
 }
