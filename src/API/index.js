747a24d6f61884ecc9393b0271003a1e14c19555
fmagmych <mychaelmagnuson@gmail.com>
2023-08-03 11:03:25 -0500

    Making sure that api helper functions export properly; making sure api requests work properly for fetching all players and a single player

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,7 @@ const cohortName = "2302-ACC-CT-WEB-PT-B";
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 const PLAYERS_API_URL = `${APIURL}/players`;
 
-export default async function fetchPlayers() {
+export async function fetchPlayers() {
   try {
     const response = await fetch(PLAYERS_API_URL);
     const data = await response.json();
@@ -13,3 +13,15 @@ export default async function fetchPlayers() {
 }
 
 fetchPlayers();
+
+export async function fetchSinglePlayer(playerId) {
+  try {
+    const response = await fetch(`${PLAYERS_API_URL}/${playerId}`);
+    const data = await response.json();
+    console.log(data.data.player);
+    return data.data.player;
+  } catch (error) {
+    console.error(error);
+  }
+}
+// fetchSinglePlayer(10733);
