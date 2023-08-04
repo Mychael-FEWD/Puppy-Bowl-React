b5e2b3c06a3c1154671589556c1bbdcb2e456c7d
fmagmych <mychaelmagnuson@gmail.com>
2023-08-04 02:21:05 -0500

    addding ids and classes to help with styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <>
-      <Link to="/">Home</Link>
-    </>
+    <header>
+      <h1 id="header">Puppy Bowl</h1>
+      <nav id="nav">
+        <Link className="link" to="/">
+          Home
+        </Link>
+      </nav>
+    </header>
   );
 }
