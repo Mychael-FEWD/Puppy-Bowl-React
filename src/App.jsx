d8bd0eb6590d7640523cf0fb4c3fa8fa6e5a4aaf
fmagmych <mychaelmagnuson@gmail.com>
2023-08-04 02:19:35 -0500

    Adding classes/ids to help with styling, adding parent tags to help with optimization and ux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AllPlayers from "./components/AllPlayers";
 import SinglePlayer from "./components/SinglePlayer";
@@ -7,25 +7,16 @@ import NewPlayerForm from "./components/NewPlayerForm";
 import "./App.css";
 
 export default function App() {
-  const [singlePlayer, setSinglePlayer] = useState([]);
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <AllPlayers
-              singlePlayer={singlePlayer}
-              onSetSinglePlayer={setSinglePlayer}
-            />
-          }
-        />
-        <Route
-          path={`/players/:id`}
-          element={<SinglePlayer singlePlayer={singlePlayer} />}
-        />
-      </Routes>
+      {" "}
+      <main className="main">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<AllPlayers />} />
+          <Route path={`/players/:id`} element={<SinglePlayer />} />
+        </Routes>
+      </main>
       <NewPlayerForm />
     </>
   );
