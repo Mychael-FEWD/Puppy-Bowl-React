1948aa5b2d83e99d7cfe59606586ec58129342fe
fmagmych <mychaelmagnuson@gmail.com>
2023-08-04 02:23:15 -0500

    Adding breed to description so that it is visible on singlePlayer.jsx render, changing the api request to be a useeffect that calls on the useParams in the singlePlayer.jsx component instead of making the request in the AllPlayers.jsx component

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,19 +1,44 @@
-export default function SinglePlayer({ singlePlayer }) {
-  function upperCaseName(string) {
-    const newName = string.charAt(0).toUpperCase() + string.slice(1);
-    return newName;
-  }
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchSinglePlayer } from "../API/index.js";
+
+export default function SinglePlayer() {
+  const [singlePlayer, setSinglePlayer] = useState([]);
+  const [error, setError] = useState(null);
+
+  const params = useParams();
+  console.log(`params: `, params, params.id);
+
+  useEffect(() => {
+    async function fetchPlayer() {
+      try {
+        const player = await fetchSinglePlayer(Number(params.id));
+        console.log(player);
+        return setSinglePlayer(player);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    fetchPlayer();
+  }, [params.id]);
 
   return (
-    <section id="single-player">
-      <div className="single-player-img-cont">
-        <img className="single-player-img" src={singlePlayer.imageUrl} />
-      </div>
-      <div className="single-player-data">
-        <h1>{upperCaseName(singlePlayer.name)}</h1>
-        <h2>Currently on-{singlePlayer.status}</h2>
-        <h2>Team: {singlePlayer.team && singlePlayer.team.name}</h2>
-      </div>
-    </section>
+    <>
+      {error && <p>{error}</p>}
+      {singlePlayer === [] ? <p>Error loading data</p> : ""}
+      {singlePlayer && (
+        <section id="single-player">
+          <div className="single-player-img-cont">
+            <img className="single-player-img" src={singlePlayer.imageUrl} />
+          </div>
+          <div className="single-player-data">
+            <h1>{singlePlayer.name}</h1>
+            <h2>{singlePlayer.breed}</h2>
+            <h2>Currently on-{singlePlayer.status}</h2>
+            <h2>Team: {singlePlayer.team && singlePlayer.team.name}</h2>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
