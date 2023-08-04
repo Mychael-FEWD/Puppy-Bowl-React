5e34fde42f9d14ec1cebbb8833bb3185084eb0e1
fmagmych <mychaelmagnuson@gmail.com>
2023-08-04 02:21:55 -0500

    Making sure everything works properly and all information is sent out correctly; adding ids for better styling in css

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -1,21 +1,49 @@
 import { useState } from "react";
+import { addNewPlayer } from "../API/index.js";
 
 export default function NewPlayerForm() {
+  const imageDefault =
+    "https://images.unsplash.com/photo-1594149929911-78975a43d4f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
+
   const [puppyName, setPuppyName] = useState("");
   const [puppyBreed, setPuppyBreed] = useState("");
   const [playerStatus, setPlayerStatus] = useState("bench");
-  const [imageUrl, setImageUrl] = useState(
-    "https://images.unsplash.com/photo-1594149929911-78975a43d4f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-  );
+  const [imageUrl, setImageUrl] = useState(imageDefault);
   const [teamId, setTeamId] = useState(739);
 
   function handleSubmit(e) {
     e.preventDefault();
+    const name = e.target[0].value;
+    const breed = e.target[1].value;
+    const status = e.target[2].value;
+    const team = Number(e.target[3].value);
+    const imageUrl = e.target[4].value;
+
+    console.log(name, breed, status, team, imageUrl);
+    async function newPlayer() {
+      try {
+        const data = await addNewPlayer(name, breed, status, imageUrl, team);
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    newPlayer();
+    setFormDefaults();
   }
+
+  function setFormDefaults() {
+    setPuppyName("");
+    setPuppyBreed("");
+    setPlayerStatus("bench");
+    setImageUrl(imageDefault);
+    setTeamId(739);
+  }
+
   return (
-    <div>
-      <h1>New Player Form</h1>
-      <form method="POST" onSubmit={handleSubmit}>
+    <section id="form-cont">
+      <h1>Add New Player</h1>
+      <form method="POST" onSubmit={handleSubmit} id="form">
         <label>
           Name:{" "}
           <input
@@ -73,6 +101,6 @@ export default function NewPlayerForm() {
 
         <button className="button">Submit</button>
       </form>
-    </div>
+    </section>
   );
 }
