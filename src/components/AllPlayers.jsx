5c6f75d5dd63b7f6a7ae774c0e61737b1cb55cf9
fmagmych <mychaelmagnuson@gmail.com>
2023-08-07 19:28:24 -0500

    Creating the search bar component, making it dynamic with a useState and making code more dry. Adding styling so the search bar works with the rest of the page design

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { fetchPlayers, deletePlayer } from "../API/index";
+import SearchBar from "./SearchBar";
 import { useNavigate } from "react-router-dom";
 
 export default function AllPlayers() {
   const [players, setPlayers] = useState([]);
+  const [query, setQuery] = useState({ search: "", results: [] });
   const [error, setError] = useState(null);
 
   const navigate = useNavigate();
@@ -33,42 +35,48 @@ export default function AllPlayers() {
     return newName;
   }
 
+  function handleMapping(allData) {
+    return allData.map((player) => (
+      <div key={player.id} className="player">
+        <div className="img-cont">
+          <img src={player.imageUrl} className="player-img" />
+        </div>
+        <div className="content-cont">
+          <div className="flex player-name-status">
+            <h3>{upperCaseName(player.name)}</h3>
+            <p>{upperCaseName(player.status)}</p>
+          </div>
+          <div className="flex player-buttons">
+            <button
+              className="details-button"
+              onClick={() => {
+                handleClick(player.id);
+              }}
+            >
+              Details
+            </button>
+            <button
+              className="delete-button"
+              onClick={() => {
+                handleDelete(player.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    ));
+  }
+
   return (
     <section>
+      <SearchBar query={query} onSetQuery={setQuery} players={players} />
       {error && <p>{error}</p>}
       <div id="players-cont">
-        {players &&
-          players.map((player) => (
-            <div key={player.id} className="player">
-              <div className="img-cont">
-                <img src={player.imageUrl} className="player-img" />
-              </div>
-              <div className="content-cont">
-                <div className="flex player-name-status">
-                  <h3>{upperCaseName(player.name)}</h3>
-                  <p>{upperCaseName(player.status)}</p>
-                </div>
-                <div className="flex player-buttons">
-                  <button
-                    className="details-button"
-                    onClick={() => {
-                      handleClick(player.id);
-                    }}
-                  >
-                    Details
-                  </button>
-                  <button
-                    className="delete-button"
-                    onClick={() => {
-                      handleDelete(player.id);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
+        {query.search === ""
+          ? handleMapping(players)
+          : handleMapping(query.results)}
       </div>
     </section>
   );
